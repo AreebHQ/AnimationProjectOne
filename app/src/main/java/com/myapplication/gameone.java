2eda46ce6fcf641ce2be984407624a4c0febcde7
Areeb Hussain <69098189+AreebHQ@users.noreply.github.com>
2021-11-11 13:21:05 -0500

    update

--- app/src/main/java/com/myapplication/gameone.java
+++ app/src/main/java/com/myapplication/gameone.java
@@ -4,23 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class gameone extends AppCompatActivity {
 
+    MyStrtDrggngLstnr mStrtDrg;
+    MyEndDrgLstnr mEndDrg;
     ImageView packImage;
     Button btn1, btn2, btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameone);
+        mStrtDrg=new MyStrtDrggngLstnr();
+        mEndDrg=new MyEndDrgLstnr();
+
+        findViewById(R.id.btn_bck).setOnLongClickListener(mStrtDrg);
+        findViewById(R.id.btn_dwn).setOnLongClickListener(mStrtDrg);
+        findViewById(R.id.btn_fwd).setOnLongClickListener(mStrtDrg);
+        findViewById(R.id.btn_up).setOnLongClickListener(mStrtDrg);
+
+        findViewById(R.id.btn_input1).setOnDragListener(mEndDrg);
+        findViewById(R.id.btn_input2).setOnDragListener(mEndDrg);
+        findViewById(R.id.btn_input3).setOnDragListener(mEndDrg);
+        findViewById(R.id.btn_input4).setOnDragListener(mEndDrg);
+
+
 
         btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
+
         packImage= (ImageView) findViewById(R.id.pack);
 
         ObjectAnimator animator = ObjectAnimator.ofFloat(packImage, "rotation", 0, 360);
@@ -37,10 +57,10 @@ public class gameone extends AppCompatActivity {
         ObjectAnimator moveRight  = ObjectAnimator.ofFloat(packImage, "translationX", 400);
         moveRight.setDuration(1500);
 
-        ObjectAnimator moveRight2  = ObjectAnimator.ofFloat(packImage, "translationX", 900);
+        ObjectAnimator moveRight2  = ObjectAnimator.ofFloat(packImage, "translationX", 800);
         moveRight2.setDuration(1500);
 
-        ObjectAnimator moveDown =  ObjectAnimator.ofFloat(packImage, "translationY", 1300);
+        ObjectAnimator moveDown =  ObjectAnimator.ofFloat(packImage, "translationY", 700);
         moveDown.setDuration(1500);
 
         ObjectAnimator hidePackman  = ObjectAnimator.ofFloat(packImage, "alpha", 1.0f, 0);
@@ -63,4 +83,41 @@ public class gameone extends AppCompatActivity {
         });
 
     }
+
+    private class MyStrtDrggngLstnr implements View.OnLongClickListener{
+
+        @Override
+        public boolean onLongClick(View v) {
+            WithDragShadow shadow= new WithDragShadow(v);
+            ClipData data= ClipData.newPlainText("","");;
+            v.startDrag(data,shadow,v,0);
+            return false;
+        }
+    }
+
+    private class MyEndDrgLstnr implements View.OnDragListener{
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            if(event.getAction()==DragEvent.ACTION_DROP){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    v.setBackground(((Button)event.getLocalState()).getBackground());
+                }
+            }
+            return true;
+        }
+    }
+
+    private class WithDragShadow extends View.DragShadowBuilder{
+
+        public WithDragShadow(View v){ super(v);  }
+
+        @Override public void onDrawShadow(Canvas canvas) { super.onDrawShadow(canvas);}
+
+        @Override
+        public void onProvideShadowMetrics(Point outShadowSize, Point outShadowTouchPoint) {
+            super.onProvideShadowMetrics(outShadowSize, outShadowTouchPoint);
+        }
+    }
+
 }
